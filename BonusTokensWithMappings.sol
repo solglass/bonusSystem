a427be64736d8ca6b1ed9b80201700728111d28c
Georgy <62201846+solglass@users.noreply.github.com>
2022-06-06 23:15:11 +0300

    Create BonusTokensWithMappings.sol

---
+++ BonusTokensWithMappings.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract BonusTokens {
+
+    mapping (address => uint256) public _numberOfTokensAccrued;
+    mapping (address => uint256) public _numberOfTokensSpent;
+    uint internal Multiplier;
+    
+    constructor() 
+    {
+        Multiplier = 1;
+    }
+
+    function currentBalance(address clientWallet) public  virtual
+        returns (uint256)
+        
+    {
+         uint256 clientBalance = _numberOfTokensAccrued[clientWallet] - _numberOfTokensAccrued[clientWallet];
+         return clientBalance;
+    }
+    
+    
+    function calculateNumberOfTokensAccrued(address clientWallet, uint256 numberOfTokensToAdd) public virtual  returns (uint256) 
+    {
+        _numberOfTokensAccrued[clientWallet] = _numberOfTokensAccrued[clientWallet] + numberOfTokensToAdd;
+        return _numberOfTokensAccrued[clientWallet];
+    }
+
+   function calculateNumberOfTokensSpent(address clientWallet,  uint256 numberOfTokensSpent, uint256 numberOfTokensToSpend) public virtual  returns (uint)  
+    {
+        _numberOfTokensSpent[clientWallet] = _numberOfTokensSpent[clientWallet] - numberOfTokensToSpend;
+        return numberOfTokensSpent;
+    }
+}
+
+contract BonusTokensPremium is BonusTokens
+{
+   constructor() 
+    {
+        Multiplier = 2;
+    }
+}
+
+contract BonusTokensVip is BonusTokens
+{
+    constructor() 
+    {
+        Multiplier = 5;
+    }   
+}
